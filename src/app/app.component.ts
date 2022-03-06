d08f9e6fddd482c779ba69ee66442d8e23d074f0
My Pc <anshu2211@rediffmail.com>
2022-03-06 12:17:31 +0530

    Second day of Angular
    DATE : - 5 / 3 / 2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'anshuman-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ANSHUMAN-HAM';
+  user='ANSHUMAN'
+  title = 'ANSHUMAN';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FOOTERComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FOOTERComponent
   ],
   imports: [
     BrowserModule
