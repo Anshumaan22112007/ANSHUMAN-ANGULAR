d16aec967d3fabec02b444522747b4cef911f97c
My Pc <anshu2211@rediffmail.com>
2022-03-13 11:42:03 +0530

    Third day of Angular
    DATE : -  6 / 3 / 2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FOOTERComponent } from './footer/footer.component';
+import { InputsComponent } from './inputs/inputs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FOOTERComponent
+    FOOTERComponent,
+    InputsComponent
   ],
   imports: [
     BrowserModule
